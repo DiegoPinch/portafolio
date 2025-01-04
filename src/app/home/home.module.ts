15ae0bb2a5569d127814299ddadaa29f4e25cd08
DIEGO PINCHA <pinchadiego8@gmail.com>
2025-01-03 22:28:03 -0500

    S realizaron cambios de los botones de redes sociales

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
-import { ButtonModule } from 'primeng/button';
-import { MenubarModule } from 'primeng/menubar';
-import { TabMenuModule } from 'primeng/tabmenu';
-import {DividerModule} from 'primeng/divider';
-import { SidebarModule } from 'primeng/sidebar';
-import { MenuModule } from 'primeng/menu';
+
 
 @NgModule({
   declarations: [
@@ -17,12 +12,6 @@ import { MenuModule } from 'primeng/menu';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    ButtonModule,
-    MenubarModule,
-    TabMenuModule,
-    DividerModule,
-    SidebarModule,
-    MenuModule
   ],
   exports: [
     HomePageComponent
